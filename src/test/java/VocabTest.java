3244190eec5261d42ea881c25b09ea2df4fd354a
NathanH34 <nhoeg001@odu.edu>
2021-10-29 10:10:44 -0400

    Gradle Pre-commit

--- src/test/java/VocabTest.java
+++ src/test/java/VocabTest.java
@@ -1,9 +1,13 @@
+package test.java;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 
 import org.junit.jupiter.api.Test;
+
+import main.java.Vocabulary;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.MatcherAssert.assertThat; 
 import static org.hamcrest.Matchers.*;
